12a4889c90857c083226e6e7ae4448e8566893cd
CodingBDX <antonycharier@gmail.com>
2022-10-16 07:10:54 +0000

    twig3

--- config/twig.php
+++ config/twig.php
@@ -1,7 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+$email = 'anto@jaicompris.fr';
+$twig->addGlobal('email', $email);
